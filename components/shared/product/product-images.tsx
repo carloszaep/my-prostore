81e4f9ad5cf7ca0de65c8a24c60627af940c8e9f
carloszaep <carlos.paez94@yahoo.com>
2025-04-29 18:50:06 -0400

    fix product page

--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -1,36 +1,37 @@
-"use client";
+'use client';
 
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import { useState } from "react";
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { useState } from 'react';
 
 const ProductImages = ({ images }: { images: string[] }) => {
   const [current, setCurrent] = useState(0);
 
   return (
-    <div className="space-y-4">
+    <div className='space-y-4'>
       <Image
         src={images[current]}
-        alt="product image"
+        alt='product image'
         width={1000}
         height={1000}
-        className="min-h-[300px] object-cover object-center"
+        className='min-h-[300px] object-cover object-center'
       />
-      <div className="flex">
+      <div className='flex'>
         {images.map((image, idx) => (
           <div
             key={idx}
             onClick={() => setCurrent(idx)}
             className={cn(
-              "border mr-2 cursor-pointer hover:border-orange-600",
-              current === idx && "border-orange-500"
+              'border rounded-sm mr-2 cursor-pointer hover:border-gray-800',
+              current === idx && 'border-gray-700'
             )}
           >
             <Image
               src={image}
-              alt="product image"
+              alt='product image'
               width={100}
               height={100}
+              className=' rounded-sm'
             />
           </div>
         ))}
